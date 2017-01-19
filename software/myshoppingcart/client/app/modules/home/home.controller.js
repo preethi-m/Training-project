78fb20890dc8a9e08c536ab82febe64e0c13c373
preethi mittapalli <preethi.mittapalli@india.semanticbits.com>
2017-01-19 18:32:12 +0530

    header and footer pages are updated

--- software/myshoppingcart/client/app/modules/home/home.controller.js
+++ software/myshoppingcart/client/app/modules/home/home.controller.js
@@ -3,16 +3,36 @@
     angular.module('app.home')
         .controller('SimpleController', SimpleController);
 
-    SimpleController.$inject = ['DataFactory'];
+    SimpleController.$inject = ['DataService','$rootScope'];
 
-    function SimpleController(DataFactory) {
-        var product = {};
-        var vm = this;
-        DataFactory.content().success(function(jsondata) {
-            vm.data = jsondata;
+    //function SimpleController(DataFactory,$rootScope) {
+    //    var product = {};
+    //    var vm = this;
+    //    DataFactory.content().success(function(jsondata) {
+    //        console.log("in controller");
+    //        vm.data = jsondata;
+    //        $rootScope.data = vm.data;
+    //    })
+    //}
+
+    function SimpleController(DataService,$rootScope) {
+        var vm =this;
+        DataService.getAllData().then(function(data) {
+
+           $rootScope.products = data;
         })
+        vm.limitNameSearch = 500; //time for displaying suggestion
+        vm.checkName = function(lettersTyped)
+        {
+
+            if(lettersTyped.length > 2)
+            {
+                vm.limitTime = 500;
+            }
+            else{
+                vm.limitTime = 0;
+            }
+        }
+
     }
-
-
-
 }());
--- software/myshoppingcart/client/app/modules/home/home.factory.js
+++ software/myshoppingcart/client/app/modules/home/home.factory.js
@@ -1,17 +1,40 @@
+//(function() {
+//    "use strict";
+//    angular.module('app.home')
+//        .factory("DataFactory", ['$http',function($http) {
+//            var obj = {};
+//            obj.content = function() {
+//                return $http.get('app.json');
+//            };
+//            return obj;
+//        }]);
+//}());
+
+
 (function() {
-"use strict";
+    "use strict";
     angular.module('app.home')
-        .factory("DataFactory", ['$http',function($http) {
-            var obj = {};
-            //console.log("in factory");
-            obj.content = function() {
-                return $http.get('app.json');
+        .service("DataService", ['$http','$q',function($http,$q) {
+                var pro = [];
+                this.getAllData = function() {
+                return $http.get('app.json', {
+                }).then(function(res) {
+
+                    return res.data;
+                },function(error){
+
+                });
+                //var def = $q.defer();
+                //
+                //$http.get("app.json")
+                //    .success(function(data) {
+                //        pro  = data;
+                //        def.resolve(data);
+                //    })
+                //    .error(function() {
+                //        def.reject("Failed to get products");
+                //    });
+                //return def.promise;
             }
-
-
-            return obj;
         }]);
-
-
 }());
-
